4a075e04677acdec85a9bf9bd7f64299b58d303c
NishuGoel <nishu0505@gmail.com>
2019-04-14 14:39:33 +0530

    Add files via upload

---
+++ src/app/create-user/create-user.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { User } from '../user-data';
+import { DataService } from '../data.service';
+import { UserFetch } from '../user-fetch';
+
+@Component({
+  selector: 'app-create-user',
+  templateUrl: './create-user.component.html',
+  styleUrls: ['./create-user.component.css']
+})
+export class CreateUserComponent implements OnInit {
+  userFormGroup: FormGroup;
+  users: User[];
+  user: UserFetch;
+
+  constructor(private dataservice: DataService){}
+  getUsers(){
+    this.dataservice.getUsers().subscribe(data => {
+      this.users = data;
+    });
+  }
+  ngOnInit() {
+    this.userFormGroup = new FormGroup(
+      {
+        name : new FormControl(''),
+        model : new FormControl('')
+      },
+    );
+    this.getUsers();
+}
+
+
+addUser() {
+  this.dataservice.addUser(this.userFormGroup.value).subscribe(data => {
+    this.user = data;
+    console.log(this.user);
+  });
+  this.getUsers();
+}
+}
