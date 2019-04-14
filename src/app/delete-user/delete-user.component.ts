441d6f4b482a47504ed8e0274a40a8ac61a1ebc0
NishuGoel <nishu0505@gmail.com>
2019-04-14 14:33:13 +0530

    Add files via upload

---
+++ src/app/delete-user/delete-user.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
+import { User } from '../user-data';
+import { UserFetch } from '../user-fetch';
+
+@Component({
+  selector: 'app-delete-user',
+  templateUrl: './delete-user.component.html',
+  styleUrls: ['./delete-user.component.css']
+})
+export class DeleteUserComponent implements OnInit {
+  users: User[];
+  user: UserFetch;
+  displayData: boolean;
+
+
+  constructor(private dataservice: DataService) { }
+
+  ngOnInit() {
+    this.getUsers();
+   // this.getUser();
+    this.deleteUser();
+  }
+ idtodelete=0;
+  deleteUser() {
+
+    this.dataservice.deleteUser(this.idtodelete).subscribe(data => {
+      this.getUsers();
+    });
+  }
+
+  getUsers(){
+    this.dataservice.getUsers().subscribe(data => {
+      this.users = data;
+    });
+  }
+  // fetchId=1;
+  // getUser() {
+  //   this.dataservice.getUser(this.fetchId).subscribe(data => {
+  //     this.user = data;
+  //    this.displayData=true;
+  //   });
+  // }
+}
