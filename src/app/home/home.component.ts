ce9873cfac115b2ee1dc9ac91ff93e22f8e461b7
NishuGoel <nishu0505@gmail.com>
2019-04-18 14:41:33 +0530

    Update home.component.ts

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,8 +4,7 @@ import { UserData } from '../user-data.service';
 
 @Component({
   selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  templateUrl: './home.component.html'
 })
 export class HomeComponent implements OnInit {
  constructor(){}
